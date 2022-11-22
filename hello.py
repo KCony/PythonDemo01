2e0092d482c24c6164117e4d16eac3aa97b53893
Jiji_Lak <julijalakova@gmail.com>
2022-11-22 12:41:57 +0300

    experimental work with "branch"

--- hello.py
+++ hello.py
@@ -1,6 +1,26 @@
-print("Hello World!")
-x = 0
+import datetime
+Str1 = {"time": ("доброй ночи", "доброе утро", "добрый день", "добрый вечер")}
+
+now = datetime.datetime.now()
+
+if 0 <= now.hour < 6:
+    greet: str = Str1["time"][0]
+if 6 <= now.hour < 12:
+    greet: str = Str1["time"][1]
+if 12 <= now.hour < 16:
+    greet: str = Str1["time"][2]
+if 16 <= now.hour < 0:
+    greet: str = Str1["time"][3]
+
+
+def talk(words): print(words)
+
+
+talk(greet.title() + " ^_*_^ " + " - замечательное время суток!")
+
+
+"""x = 0
 print("x =", x)
 y = 10
-print("y =", y)
+print("y =", y)"""
 
